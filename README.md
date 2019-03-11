b7b21278fde7b25cb02a6c041a812de3d71d26a8
Jeffrey Stone <thejeffreystone@gmail.com>
2019-03-10 20:11:08 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# To Hec With It - Tiny Splunker
+
+tohecwitit.py is a tiny python script with the single job of publishing a log event to Splunk's HTTP Event Collector.
+
+## Installation
+* Clone this repo
+ - `git clone https://github.com/thejeffreystone/to_hec_wit_it.git`
+* cd to dir `tiny_splunker`
+* install required python libraries
+ - `pip install -r requirements.txt`
+* cp env-sample to .env
+ - `cp env-sample .env`
+* Modify .env to match your environmnt:
+```
+# Splunk Server:
+splunk_server=192.168.1.20
+# Splunk HEC Port - Default is 8088:
+splunk_hec_port=8088
+# Splunk Hec SSL:
+splunk_hec_ssl=False
+# Splunk HEC Token:
+splunk_hec_key=
+# Splunk sourcetype:
+splunk_sourcetype=tiny_splunker
+# Splunk index:
+splunk_index=main
+# Splunk Source:
+splunk_source=
+# Splunk host:
+splunk_host=jarvis
+```
+
+## Usage
+
+Just call tohecwitit.py and pass arguments:
+
+```
+usage: toHecWitIt [-h] [-i IDX] [-s SRC] [-st SRCTYPE] -e EVENT
+
+Python based Splunk HEC logger
+
+required arguments:
+  -e EVENT, --event EVENT			Event to be logged in json
+
+optional arguments:
+  -h, --help            			show this help message and exit
+  -i IDX, --idx IDX     			Splunk Index you want to send the event to
+  -s SRC, --src SRC     			Source of Event
+  -st SRCTYPE, --srctype SRCTYPE 	Event Sourcetype 
+```
+Event is expected to be in json. Since the log event is passed to the Splunk HTTP Event Collector as json, the sourcetype can be anything. Splunk will auto-extract the key value pairs.
+
+If none of the optional requirements are passed the script will use the values set in the .env file.
+
+### Example:
+
+`./toHecWitIt.py -i main -e '{"log_level":"info","action":"Update","message":"User has been added"}'`
+
+## Compatibility
+
+This script was written and tested using python 3.7.2
+
+
---
+++ tohecwitit.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+###############################################################################
+#   @author         :   Jeffrey Stone 
+#   @date           :   03/08/2019
+#   @script        	:   toHecWitIt.py
+#   @description    :   Simple Python script to send events to Splunk when envoked. AKA Tiny Splunker. 
+###############################################################################
+
+import os
+import time
+import sys
+import argparse
+import json
+from splunk_http_event_collector import http_event_collector
+from dotenv import load_dotenv
+load_dotenv()
+
+app_mode = os.getenv("app_mode")
+
+http_event_collector_key = os.getenv("splunk_hec_key")
+http_event_collector_host = os.getenv("splunk_server")
+http_event_collector_ssl = os.getenv("splunk_hec_ssl")
+http_event_collector_port = int(os.getenv("splunk_hec_port"))
+splunk_host = os.getenv("splunk_host")
+splunk_source = os.getenv("splunk_source")
+splunk_sourcetype = os.getenv("splunk_sourcetype")
+splunk_index = os.getenv("splunk_index")
+
+parser = argparse.ArgumentParser(prog='toHecWitIt',epilog='toHecWitIt is part of Tiny Splunker',description='Python based Splunk HEC logger')
+parser.add_argument('-i','--idx',help='Splunk Index', required=False)
+parser.add_argument('-s','--src',help='Source of Event', required=False)
+parser.add_argument('-st','--srctype',help='Event Sourcetype', required=False)
+parser.add_argument('-e','--event',help='Event to be logged in json', type=json.loads, required=True)
+args = parser.parse_args()
+
+if http_event_collector_ssl == "False":
+	http_event_collector_ssl = False
+else:
+	http_event_collector_ssl = True
+
+# Get Args
+if args.idx:
+	splunk_index = args.idx
+else:
+	splunk_index = os.getenv("splunk_index")
+
+if args.src:
+	splunk_source = args.src
+else:
+	splunk_source = os.getenv("splunk_source")
+
+if args.srctype:
+	splunk_sourcetype = args.srctype
+else:
+	splunk_sourcetype = os.getenv("splunk_sourcetype")
+
+def splunkIt():
+	# Splunk it
+	logevent = http_event_collector(http_event_collector_key, http_event_collector_host, http_event_port = http_event_collector_port, http_event_server_ssl = http_event_collector_ssl)
+	logevent.popNullFields = True
+
+	payload = {}
+	payload.update({"index":splunk_index})
+	payload.update({"sourcetype":splunk_sourcetype})
+	payload.update({"source":splunk_source})
+	payload.update({"host":splunk_host})
+	payload.update({"event":args.event})
+
+	logevent.sendEvent(payload)
+	logevent.flushBatch()
+	print(payload)
+
+def main():
+	splunkIt()
+
+if __name__ == "__main__":
+    main()
+
